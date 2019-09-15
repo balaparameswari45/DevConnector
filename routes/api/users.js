09d5df0809cc3cfaa8d951f0fc05a48301a8c040
balaparameswari45 <52223968+balaparameswari45@users.noreply.github.com>
2019-09-15 12:24:40 -0700

    password encryption  and user register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router =express.Router();
 const User =require('../../models/User');
 
+const gravatar =require('gravatar');
+const bcrypt =require('bcryptjs');
+
 // @route  POST api/users/register
 // @desc Register a user
 // @access Public
@@ -11,15 +14,58 @@ router.post('/register',(req,res) => {
   .then(user => {
     if (user){
       return res.status(400).json({email:'Email already exists'})
-    } 
-    const newUser = new User ({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    })
+    } else {
+        const avatar = gravatar.url(req.body.email,{
+          s:'200',
+          r:'pg',
+          d: 'mm'
+        });
+
+        const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10,(err,salt) => {
+        if(err) throw err;
+        bcrypt.hash(newUser.password, salt,(err,hash) => {
+          if(err) throw err;
+          newUser.password =hash;
+          newUser.save()
+          .then( user => res.json(user))
+          .catch(err => console.log(err));
+        })
+      })
+    }
+    
   })
   .catch(err => console.log(err));
 })
+
+// @route  POST api/users/login
+// @desc login a user/ return JWT token
+// @access Public
+
+
+router.post('/login',(req, res) => {
+  const email =req.body.email;
+  const password = req.body.password;
+
+  //Find user by  email
+
+  User.findOne({email})
+  .then(user => {
+    if (!user){
+      return res.status(404).json({
+        email:'User not found'
+      });
+    }
+  })
+  .catch(err => console.log(err));
+})
+
 
 
 
