3e456ebfda9bc432ee198756ef8ac889f0e456b4
balaparameswari45 <52223968+balaparameswari45@users.noreply.github.com>
2019-09-22 22:30:23 -0700

    Register,login, encryption and authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,75 +1,132 @@
-const express = require('express');
-const router =express.Router();
-const User =require('../../models/User');
+const express = require("express");
 
-const gravatar =require('gravatar');
-const bcrypt =require('bcryptjs');
+//We don't need all of express in this file, just the Router
+const router = express.Router();
 
-// @route  POST api/users/register
-// @desc Register a user
-// @access Public
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
-router.post('/register',(req,res) => {
-  User.findOne({email:req.body.email})
-  .then(user => {
-    if (user){
-      return res.status(400).json({email:'Email already exists'})
-    } else {
-        const avatar = gravatar.url(req.body.email,{
-          s:'200',
-          r:'pg',
-          d: 'mm'
+// @route   POST api/users/register
+// @desc    Register a user
+// @access  Public
+router.post("/register", (req, res) => {
+  const {errors, isValid} =validateRegisterInput(req.body);
+
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      errors.email = "Email already exists";
+      if (user) {
+        return res.status(400).json(errors);
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm"
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
         });
 
-        const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      });
-
-      bcrypt.genSalt(10,(err,salt) => {
-        if(err) throw err;
-        bcrypt.hash(newUser.password, salt,(err,hash) => {
-          if(err) throw err;
-          newUser.password =hash;
-          newUser.save()
-          .then( user => res.json(user))
-          .catch(err => console.log(err));
-        })
-      })
-    }
-    
-  })
-  .catch(err => console.log(err));
+        //separate function
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    })
+    .catch(err => console.log(err));
 })
 
-// @route  POST api/users/login
-// @desc login a user/ return JWT token
-// @access Public
+// @route   POST api/users/login
+// @desc    Login user / return JWT token
+// @access  Public
 
-
-router.post('/login',(req, res) => {
-  const email =req.body.email;
+router.post('/login', (req, res) => {
+  const email = req.body.email;
   const password = req.body.password;
 
-  //Find user by  email
+  const {errors, isValid } =validateLoginInput(req.body);
 
-  User.findOne({email})
-  .then(user => {
-    if (!user){
-      return res.status(404).json({
-        email:'User not found'
-      });
-    }
-  })
-  .catch(err => console.log(err));
-})
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
 
+    //Find user by email
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+
+        errors.password = 'User not found';
+        return res.status(404).json(errors);
+      }
+
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if (!isMatch){
+
+          errors.password = "password doesnot match";
+          return res.status(400).json(errors);
+        }
+
+        const payload = {
+          id:user.id,
+          name:user.name,
+          avatar:user.avatar
+        };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {expiresIn:3600},
+          (err,token) => {
+            if (err) throw err;
+
+            return res.json({
+              success:true,
+              token: 'Bearer ' + token
+          });
+          }
+          );
+        
+      })
+      .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+
+});
 
 
+// @route   GET api/users/current
+// @desc    return current user
+// @access  Private
 
 
+router.get('/current', 
+ passport.authenticate('jwt', {session:false}),
+(req,res) => {
+  res.json({
+    id: req.user.id,
+    email:req.user.email,
+    name: req.user.name
+  });
+});
 
-module.exports =router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,44 @@
-const express = require('express');
-const mongoose =require ('mongoose');
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport  = require('passport');
+
+//create instance of express
 const app = express();
 
-
 //Body parser middleware
-
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-//db config
-const db =require('./config/keys').mongoURI;
+//passport configuration
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+//Db config
+//connection string in keys file
+const db = require("./config/keys").mongoURI;
 
 //connect to mongodb
-mongoose.connect(db)
-.then(() =>console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(db)
+  //prmoise statement
+  .then(() => console.log("MongoDb connected"))
+  .catch(err => console.log(err));
 
-//First route
-app.get('/', (req,res) => res.send('hello'))
-app.use('/api/users',users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+//create first route
+//allow user to get data back
+app.get("/", (req, res) => res.send("hello"));
 
-const port =7000;
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-app.listen(port,()=> console.log(`server running on port ${port}`));
+const port = 7000;
+
+//tell express to listen on this port
+app.listen(port, () => console.log(`Server running on port ${port}`));
